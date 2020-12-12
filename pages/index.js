b067a8e9d7a4b333abd7c84df8e7f022b5051a0b
Pancho Urgiles <panchourgiles@gmail.com>
2020-12-12 10:36:37 -0500

    Fetch data from the server and render the list of popular routes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
-import Subtitle from 'components/Subtitle';
-import Title from 'components/Title';
+import fetchPopularRoutes from '@api/popularRoutes';
+import Layout from '@commonComponents/Layout';
+import ItemPopularRoute from '@components/ItemPopularRoute';
+import ListPopularRoute from '@components/ListPopularRoute';
+import { nanoid } from 'nanoid';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const popularRoutes = await fetchPopularRoutes();
+
+  return {
+    props: {
+      popularRoutes
+    }
+  };
+};
+
+export default function Home({ popularRoutes }) {
   return (
-    <>
-      <Title>Hello world!!!</Title>
-      <Subtitle>With NEXT.js and styled-components</Subtitle>
-    </>
+    <Layout>
+      <ListPopularRoute>
+        {popularRoutes.map((popularRoute) => {
+          return (
+            <ItemPopularRoute
+              key={nanoid(8)}
+              routeCoverImage={popularRoute.routeCoverImage}
+              origin={popularRoute.origin}
+              destination={popularRoute.destination}
+              departureDate={popularRoute.departureDate}
+              priceUSD={popularRoute.priceUSD}
+              tripType={popularRoute.tripType}
+            />
+          );
+        })}
+      </ListPopularRoute>
+    </Layout>
   );
 }
