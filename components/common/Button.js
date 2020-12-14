e982304cf763734387c034219cb84edb2445985f
Pancho Urgiles <panchourgiles@gmail.com>
2020-12-14 00:11:54 -0500

    Improve styles in Button component

--- components/common/Button.js
+++ components/common/Button.js
@@ -1,8 +1,10 @@
-import { colors, fonts, spacing } from '@styles/theme';
+import { colors, fonts, spacing } from '@styles';
 import styled from 'styled-components';
 
-const Button = ({ children, onClick }) => (
-  <Wrapper onClick={onClick}>{children}</Wrapper>
+const Button = ({ children, type = 'button', onClick, ...rest }) => (
+  <Wrapper type={type} onClick={onClick} {...rest}>
+    {children}
+  </Wrapper>
 );
 
 const Wrapper = styled.button`
@@ -13,8 +15,12 @@ const Wrapper = styled.button`
   cursor: pointer;
   display: inline-block;
   font-size: ${fonts.size.medium};
+  font-weight: bold;
   padding: ${spacing.small} ${spacing.medium};
   text-transform: uppercase;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 export default Button;
