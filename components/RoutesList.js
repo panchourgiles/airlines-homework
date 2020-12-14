869583b91a738ec2755a214a91eb8496da927b77
Pancho Urgiles <panchourgiles@gmail.com>
2020-12-13 23:55:38 -0500

    Rename variables and refactor some code

--- components/RoutesList.js
+++ components/RoutesList.js
@@ -1,10 +1,13 @@
 import RoutesItem from '@components/RoutesItem';
+import SearchForm from '@components/SearchForm';
+import { useModal } from '@context/ModalContext';
 import { useDispatchRoute } from '@context/RoutesContext';
 import { nanoid } from 'nanoid';
 import styled from 'styled-components';
 
-const RoutesList = ({ popularRoutes }) => {
+const RoutesList = ({ routes }) => {
   const dispatch = useDispatchRoute();
+  const { handleModal } = useModal();
 
   const handleRoute = ({ origin, destination, departureDate, tripType }) => {
     dispatch({
@@ -16,26 +19,30 @@ const RoutesList = ({ popularRoutes }) => {
         tripType
       }
     });
+
+    const origins = routes.map((route) => route.origin);
+    const destinations = routes.map((route) => route.destination);
+    handleModal(<SearchForm origins={origins} destinations={destinations} />);
   };
 
   return (
     <Wrapper>
-      {popularRoutes.map((popularRoute) => {
+      {routes.map((route) => {
         return (
           <RoutesItem
             key={nanoid(8)}
-            routeCoverImage={popularRoute.routeCoverImage}
-            origin={popularRoute.origin}
-            destination={popularRoute.destination}
-            departureDate={popularRoute.departureDate}
-            priceUSD={popularRoute.priceUSD}
-            tripType={popularRoute.tripType}
+            routeCoverImage={route.routeCoverImage}
+            origin={route.origin}
+            destination={route.destination}
+            departureDate={route.departureDate}
+            priceUSD={route.priceUSD}
+            tripType={route.tripType}
             onClick={() =>
               handleRoute({
-                origin: popularRoute.origin,
-                destination: popularRoute.destination,
-                departureDate: popularRoute.departureDate,
-                tripType: popularRoute.tripType
+                origin: route.origin,
+                destination: route.destination,
+                departureDate: route.departureDate,
+                tripType: route.tripType
               })
             }
           />
