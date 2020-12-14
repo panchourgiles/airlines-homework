8b406bf260c75ab0579912e88d02250bc2e8a40c
Pancho Urgiles <panchourgiles@gmail.com>
2020-12-14 00:10:52 -0500

    Create RadioInput component

---
+++ components/common/RadioInput.js
@@ -0,0 +1,33 @@
+import { fonts, spacing } from '@styles';
+import styled from 'styled-components';
+
+const RadioInput = ({ id, label, name, checked = false, value, onChange }) => {
+  return (
+    <Wrapper>
+      <Radio
+        id={id}
+        name={name}
+        type="radio"
+        checked={checked}
+        value={value}
+        onChange={onChange}
+      />
+      {label && <Label htmlFor={id}>{label}</Label>}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: inline-block;
+  margin-right: ${spacing.xsmall};
+`;
+
+const Radio = styled.input`
+  margin: 0.4rem;
+`;
+
+const Label = styled.label`
+  font-size: ${fonts.size.medium};
+`;
+
+export default RadioInput;
