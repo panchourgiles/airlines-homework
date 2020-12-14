527363db5046ede0874359ddff01f3da34e39b25
Pancho Urgiles <panchourgiles@gmail.com>
2020-12-14 00:14:30 -0500

    Add page to show the search resutls

---
+++ pages/search.js
@@ -0,0 +1,10 @@
+import Layout from '@commonComponents/Layout';
+import ResultList from '@components/ResultList';
+
+export default function Search() {
+  return (
+    <Layout>
+      <ResultList />
+    </Layout>
+  );
+}
