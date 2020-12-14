782d20d5bf570dc259c8f90a3338ed22f1fdb03a
Pancho Urgiles <panchourgiles@gmail.com>
2020-12-14 13:06:08 -0500

    Add title in index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import fetchPopularRoutes from '@api/popularRoutes';
 import Layout from '@commonComponents/Layout';
+import Title from '@commonComponents/Title';
 import RoutesList from '@components/RoutesList';
 
 export const getServerSideProps = async () => {
@@ -14,6 +15,7 @@ export const getServerSideProps = async () => {
 export default function Home({ popularRoutes }) {
   return (
     <Layout>
+      <Title textAlign={'center'}>Welcome to Airlines Homework!</Title>
       <RoutesList routes={popularRoutes} />
     </Layout>
   );
